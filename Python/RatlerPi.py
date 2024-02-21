f7d0c71f0e23952f77db3d388af1015f13a7f869
MattRSON <mattrawson02@outlook.com>
2024-02-21 00:53:21 -0700

    Update RatlerPi.py

--- Python/RatlerPi.py
+++ Python/RatlerPi.py
@@ -19,6 +19,9 @@ spi.max_speed_hz = 1000000 #1Mhz
 spi.mode = 0
 
 while(1):
+    print('t1')
     data = input("Enter 1 or 0: ")
+    print('t2')
     spi.writebytes(int(data))
+    print('t3')
             
