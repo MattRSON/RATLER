84fb1d2bd2706fa2da59dd041b3acdd70029020b
MattRSON <mattrawson02@outlook.com>
2024-02-26 11:11:38 -0700

    interrupt test

--- MicroController.c
+++ MicroController.c
@@ -1,33 +1,44 @@
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define   SET_BIT(Reg, Bit) (Reg |=  (1 << (Bit)))
 #define CLEAR_BIT(Reg, Bit) (Reg &= ~(1 << (Bit)))
 #define  FLIP_BIT(Reg, Bit) (Reg ^=  (1 << (Bit)))
 #define  TEST_BIT(Reg, Bit) ((Reg >> (Bit)) & 1)
 
+volatile int data = 0;
 void SPI_SlaveInit(void) {
     // Set MISO as output
     DDRB |= (1<<DDB4);
     // Enable SPI
-    SPCR = (1<<SPE);
+    SET_BIT(SPCR, SPE);
+    SET_BIT(SPCR,SPIE);
 }
 
+/*
 char SPI_Receive(void) {
     // Wait for reception to complete
     while(!(SPSR & (1<<SPIF))) {}
     //return data register
     return SPDR;
 }
+*/
+ISR (SPIF_vector){
+    data = SPDR;
+}
 
 int main(void) {
 
     SPI_SlaveInit;
-
+    sei();
     DDRB |= (1<<DDB0);
-    int data = 0;
-
+    // try an spi interupt
     while (1) {
-        data = (int) SPI_Receive;
+        //data = (int) SPI_Receive;
         if (data == 1) {
             SET_BIT(PORTB,DDB0);
         }
