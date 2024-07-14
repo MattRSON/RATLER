af666b84a7f385014b6337e26ececb5975065c42
Mathew <mattrawson02@outlook.com>
2024-07-14 16:46:12 -0600

    Test

--- MicroController.c
+++ MicroController.c
@@ -2,6 +2,23 @@
 #define F_CPU 8000000UL
 #endif
 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+ int main()
+ {
+     // Set built-in LED pin as output
+     DDRB |= (1 << DDB0);
+     while (1) {
+         PORTB |=  (1 << PB0);   // LED on
+         _delay_ms(10);         // wait 500ms
+         PORTB &= ~(1 << PB0);   // LED off
+         _delay_ms(10);         // wait 500ms
+     }
+     return 0;
+ }
+/*
 #include <avr/io.h>
 //#include <avr/interrupt.h>
 
@@ -48,3 +65,4 @@ int main(void) {
 
 
 }
+*/
