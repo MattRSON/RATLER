2fa13aa7a10997f2ea7fd4a5257f98b0c4ec7a1a
Mathew <mattrawson02@outlook.com>
2024-07-15 18:03:08 -0600

    Create SPIC.c

---
+++ Test Codes/SPIC.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bcm2835.h>
+
+int main(int argc, char** argv) {
+
+ if (!bcm2835_init()) {
+  return 1;
+ }
+ if (!bcm2835_spi_begin()) {
+  return 1;
+ }
+
+ bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
+ bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
+ bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536);
+ bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+ bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
+
+ uint8_t read_data = bcm2835_spi_transfer(0xAA);
+
+ if( read_data== 0xAA) printf("data received correctly");
+
+ bcm2835_spi_end();
+ bcm2835_close();
+ return (EXIT_SUCCESS);
+}
