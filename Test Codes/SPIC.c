c25edc2ad87c1ccef569afa51e663810d37e859f
Mathew <mattrawson02@outlook.com>
2024-07-15 18:34:05 -0600

    Update SPIC.c

--- Test Codes/SPIC.c
+++ Test Codes/SPIC.c
@@ -10,10 +10,11 @@ int main(){
     if (gpioInitialise()<0) return -1;
 
 
-    unsigned char DataTX[1] = 1;
+    unsigned char DataTX[1];
     unsigned char DataRX[1];
     int handle = spiOpen(1, 1000000, 0);
 
+    DataTX[0] = 1;
 
     while(1){
         spiXfer(handle, DataTX, DataRX, 1);
@@ -22,7 +23,6 @@ int main(){
    
 
     // Terminate the library
-    fclose(fp);
     spiClose(handle);
     gpioTerminate();
     return 0;
