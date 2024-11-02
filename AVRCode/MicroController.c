ef9896ac6e235422ef3b354e53b373ab7af27d6d
Mathew <mattrawson02@outlook.com>
2024-11-02 17:02:34 -0600

    SPI test

--- AVRCode/MicroController.c
+++ AVRCode/MicroController.c
@@ -2,7 +2,6 @@
 #define F_CPU 8000000UL
 #endif
 
-/*
 #include <avr/io.h>
 //#include <avr/interrupt.h>
 #include <util/delay.h>
@@ -52,34 +51,3 @@ int main(void) {
 
 
 }
-*/
-#include <avr/io.h>
-#include <util/delay.h>
-
-#define   SET_BIT(Reg, Bit) (Reg |=  (1 << (Bit)))
-#define CLEAR_BIT(Reg, Bit) (Reg &= ~(1 << (Bit)))
-#define  FLIP_BIT(Reg, Bit) (Reg ^=  (1 << (Bit)))
-#define  TEST_BIT(Reg, Bit) ((Reg >> (Bit)) & 1)
-
- int main()
- {
-     // Set built-in LED pin as output
-     SET_BIT(DDRB,DDB0);
-     CLEAR_BIT(DDRB,DDB1);
-     SET_BIT(PORTB,PB1); //Pull ups
-     while (1) {
-        if (TEST_BIT(PINB,PB1)) {
-            SET_BIT(PORTB,PB0);
-        }   
-        else {
-            CLEAR_BIT(PORTB,PB0);
-        }
-        /*
-         SET_BIT(PORTB,PB0);        // LED on
-         _delay_ms(500);         // wait 500ms
-         CLEAR_BIT(PORTB,PB0);      // LED off
-         _delay_ms(500);         // wait 500ms
-         */
-     }
-     return 0;
- }
--- Test Codes/ButtonTest.c
+++ Test Codes/ButtonTest.c
@@ -1,15 +1,30 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define   SET_BIT(Reg, Bit) (Reg |=  (1 << (Bit)))
+#define CLEAR_BIT(Reg, Bit) (Reg &= ~(1 << (Bit)))
+#define  FLIP_BIT(Reg, Bit) (Reg ^=  (1 << (Bit)))
+#define  TEST_BIT(Reg, Bit) ((Reg >> (Bit)) & 1)
+
  int main()
  {
      // Set built-in LED pin as output
-     DDRB |= (1 << DDB0);
+     SET_BIT(DDRB,DDB0);
+     CLEAR_BIT(DDRB,DDB1);
+     SET_BIT(PORTB,PB1); //Pull ups
      while (1) {
-         PORTB |=  (1 << PB0);   // LED on
-         _delay_ms(10);         // wait 500ms
-         PORTB &= ~(1 << PB0);   // LED off
-         _delay_ms(10);         // wait 500ms
+        if (TEST_BIT(PINB,PB1)) {
+            SET_BIT(PORTB,PB0);
+        }   
+        else {
+            CLEAR_BIT(PORTB,PB0);
+        }
+        /*
+         SET_BIT(PORTB,PB0);        // LED on
+         _delay_ms(500);         // wait 500ms
+         CLEAR_BIT(PORTB,PB0);      // LED off
+         _delay_ms(500);         // wait 500ms
+         */
      }
      return 0;
  }
