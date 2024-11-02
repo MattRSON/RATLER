534ae11370fea613e70938d00b9a42341bba50ad
Mathew <mattrawson02@outlook.com>
2024-11-02 16:22:30 -0600

    Test

--- AVRCode/MicroController.c
+++ AVRCode/MicroController.c
@@ -2,6 +2,7 @@
 #define F_CPU 8000000UL
 #endif
 
+/*
 #include <avr/io.h>
 //#include <avr/interrupt.h>
 #include <util/delay.h>
@@ -51,18 +52,19 @@ int main(void) {
 
 
 }
-// #include <avr/io.h>
-// #include <util/delay.h>
+*/
+#include <avr/io.h>
+#include <util/delay.h>
 
-//  int main()
-//  {
-//      // Set built-in LED pin as output
-//      DDRB |= (1 << DDB0);
-//      while (1) {
-//          PORTB |=  (1 << PB0);   // LED on
-//          _delay_ms(10);         // wait 500ms
-//          PORTB &= ~(1 << PB0);   // LED off
-//          _delay_ms(10);         // wait 500ms
-//      }
-//      return 0;
-//  }
+ int main()
+ {
+     // Set built-in LED pin as output
+     DDRB |= (1 << DDB0);
+     while (1) {
+         PORTB |=  (1 << PB0);   // LED on
+         _delay_ms(10);         // wait 500ms
+         PORTB &= ~(1 << PB0);   // LED off
+         _delay_ms(10);         // wait 500ms
+     }
+     return 0;
+ }
