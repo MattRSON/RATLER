a8db602b71bfc5130d5874013b53577bf91961f0
MattRSON <mattrawson02@outlook.com>
2026-02-10 20:00:40 -0600

    Blink Test

--- Test Codes/Blink.c
+++ Test Codes/Blink.c
@@ -7,9 +7,9 @@
      DDRB |= (1 << DDB0);
      while (1) {
          PORTB |=  (1 << PB0);   // LED on
-         _delay_ms(10);         // wait 500ms
+         _delay_ms(1000);         // wait 500ms
          PORTB &= ~(1 << PB0);   // LED off
-         _delay_ms(10);         // wait 500ms
+         _delay_ms(1000);         // wait 500ms
      }
      return 0;
  }
