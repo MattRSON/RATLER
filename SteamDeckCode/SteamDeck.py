d1fdf1c5cdee615e211e70cf45dea31ef908c2ff
MattRSON <mattrawson02@outlook.com>
2026-02-19 23:10:13 -0600

    Update SteamDeck.py

--- SteamDeckCode/SteamDeck.py
+++ SteamDeckCode/SteamDeck.py
@@ -1,11 +1,12 @@
 import socket
 
-RPi_IP = "192.168.1.75"   # <-- put your Pi IP here
+#RPi_IP = "192.168.1.75"   # <-- put your Pi IP here
+RPi_HOST = "RATLER"
 PORT = 5000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 message = "Hello from Steam Deck"
-sock.sendto(message.encode(), (RPi_IP, PORT))
+sock.sendto(message.encode(), (RPi_HOST, PORT))
 
 print("Message sent!")
