5fddf73c622d33d0d7b8a6bdf801c38e9617d566
MattRSON <mattrawson02@outlook.com>
2026-02-19 23:13:20 -0600

    UDP

--- PiCode/SPIC.c
+++ PiCode/SPIC.c
@@ -1,39 +1,68 @@
-// simple_udp_server.c
+// udp_server.c
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <unistd.h>
+#include <sys/time.h>
 
 #define PORT 5000
+#define PACKET_SIZE 5
+#define TIMEOUT_MS 200
 
 int main() {
     int sockfd;
-    char buffer[1024];
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
+    uint8_t buffer[PACKET_SIZE];
 
-    // Create socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    // Setup server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Bind socket
     bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-    printf("Waiting for message on port %d...\n", PORT);
+    printf("UDP Server listening on port %d\n", PORT);
 
-    // Receive message
-    int n = recvfrom(sockfd, buffer, sizeof(buffer)-1, 0,
-                     (struct sockaddr*)&client_addr, &addr_len);
+    struct timeval last_packet, now;
+    gettimeofday(&last_packet, NULL);
 
-    buffer[n] = '\0';   // Null-terminate
+    while (1) {
 
-    printf("Received: %s\n", buffer);
+        int n = recvfrom(sockfd, buffer, PACKET_SIZE, MSG_DONTWAIT,
+                         (struct sockaddr*)&client_addr, &addr_len);
+
+        if (n == PACKET_SIZE) {
+            gettimeofday(&last_packet, NULL);
+
+            int8_t m1 = (int8_t)buffer[0];
+            int8_t m2 = (int8_t)buffer[1];
+            int8_t m3 = (int8_t)buffer[2];
+            int8_t m4 = (int8_t)buffer[3];
+            uint8_t buttons = buffer[4];
+
+            printf("M1:%d M2:%d M3:%d M4:%d BTN:%u\n",
+                   m1, m2, m3, m4, buttons);
+
+            // TODO: forward to SPI here
+        }
+
+        gettimeofday(&now, NULL);
+        long elapsed_ms =
+            (now.tv_sec - last_packet.tv_sec) * 1000 +
+            (now.tv_usec - last_packet.tv_usec) / 1000;
+
+        if (elapsed_ms > TIMEOUT_MS) {
+            // Watchdog: stop motors
+            // send zero speeds to SPI
+        }
+
+        usleep(1000);
+    }
 
     close(sockfd);
     return 0;
--- SteamDeckCode/SteamDeck.py
+++ SteamDeckCode/SteamDeck.py
@@ -1,12 +1,23 @@
 import socket
+import time
+import struct
 
-#RPi_IP = "192.168.1.75"   # <-- put your Pi IP here
+#RPi_IP = "192.168.1.75"   # <-- change to your Pi IP
 RPi_HOST = "RATLER"
 PORT = 5000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-message = "Hello from Steam Deck"
-sock.sendto(message.encode(), (RPi_HOST, PORT))
+while True:
+    # Example motor values
+    m1 = 50      # forward
+    m2 = -30     # reverse
+    m3 = 0
+    m4 = 0
+    buttons = 0b00000001
 
-print("Message sent!")
+    packet = struct.pack("bbbbB", m1, m2, m3, m4, buttons)
+
+    sock.sendto(packet, (RPi_HOST, PORT))
+
+    time.sleep(1/60)   # 60 Hz
