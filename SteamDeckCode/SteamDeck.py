f2afa2d2f544ab9434fc94c6aba31852053ddb49
MattRSON <mattrawson02@outlook.com>
2026-02-20 00:19:16 -0600

    Update SteamDeck.py

--- SteamDeckCode/SteamDeck.py
+++ SteamDeckCode/SteamDeck.py
@@ -24,20 +24,20 @@ while True:
     # Example stick values
     x1 = joystick.get_axis(0)
     y1 = joystick.get_axis(1)
-    x2 = joystick.get_axis(2)
+    x2 = joystick.get_axis(4)
     y2 = joystick.get_axis(3)
     buttons = 0b00000001
 
-    #packet = struct.pack("bbbbB", x1, y1, x2, y2, buttons)
+    packet = struct.pack("eeeeB", x1, y1, x2, y2, buttons)
 
-    #sock.sendto(packet, (RPi_HOST, PORT))
+    sock.sendto(packet, (RPi_HOST, PORT))
 
-    left_x_axis = joystick.get_axis(0)
-    left_y_axis = joystick.get_axis(1)
-    right_x_axis = joystick.get_axis(4)
-    right_y_axis = joystick.get_axis(3)
+    #left_x_axis = joystick.get_axis(0)
+    #left_y_axis = joystick.get_axis(1)
+    #right_x_axis = joystick.get_axis(4)
+    #right_y_axis = joystick.get_axis(3)
             
     # Axis values range from -1.0 to 1.0
-    print(f"Left Stick X: {left_x_axis:>6.3f}, Y: {left_y_axis:>6.3f} | Right Stick X: {right_x_axis:>6.3f}, Y: {right_y_axis:>6.3f}")
+    #print(f"Left Stick X: {left_x_axis:>6.3f}, Y: {left_y_axis:>6.3f} | Right Stick X: {right_x_axis:>6.3f}, Y: {right_y_axis:>6.3f}")
 
     time.sleep(1/60)   # 60 Hz
